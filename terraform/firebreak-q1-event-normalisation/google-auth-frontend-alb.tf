2ebc1c85d0e990a86856bac6c2420dd66f4bd825
emisaur <emily.young@digital.cabinet-office.gov.uk>
2019-04-05 14:35:34 +0100

    Adding redirect HTTP to HTTPS.

--- terraform/firebreak-q1-event-normalisation/google-auth-frontend-alb.tf
+++ terraform/firebreak-q1-event-normalisation/google-auth-frontend-alb.tf
@@ -42,6 +42,22 @@ resource "aws_lb_listener" "event-normalisation-listner" {
   }
 }
 
+resource "aws_lb_listener" "event-normalisation-listner_80" {
+  load_balancer_arn = "${aws_lb.event-normalisation-alb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_lb_listener_rule" "route_auth" {
   listener_arn = "${aws_lb_listener.event-normalisation-listner.arn}"
   priority     = 100
--- terraform/firebreak-q1-event-normalisation/network.tf
+++ terraform/firebreak-q1-event-normalisation/network.tf
@@ -40,6 +40,14 @@ resource "aws_security_group" "event-normalisation-alb-ingress" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTP in from everywhere
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags {
     Name      = "event-normalisation-alb-sg-ingress"
     Product   = "security-group"
